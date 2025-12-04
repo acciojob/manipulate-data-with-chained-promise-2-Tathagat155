e2885c13f8261c3a4e9eb71cb96230dbd7298d00
Tathagat155 <tathagatkumar70618@gmail.com>
2025-12-04 12:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 //your JS code here. If required.
 let arr=[1,2,3,4];
 const div=document.getElementById("output");
+
 const promise= new Promise((resolve,reject)=>{
-	     let filterArr=arr.filter((elem)=>elem%2==0);
-	     
-	   setTimeout(()=>resolve(filterArr.join(",")),1000);
+	     let filterArr=arr.filter((elem)=>elem%2==0);     
+	   setTimeout(()=>resolve(filterArr),1000);
 }).then((arr)=>{
-	 div.textContent=arr;
-	 let newArr=arr.map((elem)=>{
-		 return elem*2;
-	 });
-	const promise=new promise((resolve,reject)=>{
-		setTimeout(()=>resolve(newArr.join(",")),2000);
+	 div.textContent=arr.join(",");
+	 let newArr=arr.map(elem=> elem*2);
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>resolve(newArr),2000);
 	})
-}.then((newArr)=>{
-	div.textContent=newArr;
-}
+}).then((newArr)=>{
+	div.textContent=newArr.join(",");
+});
